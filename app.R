1179ffc7066a1f9de5a27e7505b7fde95caea8a7
Pigmoth69 <pigmoth@gmail.com>
2018-01-18 04:11:47 +0100

    beautifull plotly

--- app.R
+++ app.R
@@ -1,17 +1,26 @@
 library(shiny)
-library(ggplot2)
-library(ggmap)
-library(maps)
-library(mapdata)
-library(dplyr)
+library(plotly)
+library(openintro)
 
 #load the dataset
-arrests <- read.csv(file = "dataset/USArrests.csv",header=TRUE, sep = ",")
+df <- read.csv("dataset/USArrests.csv")
+df["Code"] <- state2abbr(df$Region)
 
-arr <- arrests %>% add_rownames("region") %>% mutate(region=tolower(Region))
+#define hoover
+df$hover <- with(df, paste(Region, '<br>', "Murder", Murder, "Assault", Assault, "<br>","Urban Population",UrbanPop,"<br>","Rape",Rape))
 
-usa <- map_data("usa")
-states <- map_data("state")
+# give state boundaries a white border
+l <- list(color = toRGB("white"), width = 2)
+
+# Specify map projection and options
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = toRGB('white')
+)
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -23,10 +32,10 @@ ui <- fluidPage(
       sidebarPanel(
         helpText("Create european maps with 
                general information of the countries."),
-        selectInput("var", 
-                    label = "Choose a variable to display",
-                    choices = names(arrests)[2:5]),
-         sliderInput("bins",
+        #selectInput("var", 
+        #            label = "Choose a variable to display",
+        #            choices = names(arrests)[2:5]),
+        sliderInput("bins",
                      "Number of bins:",
                      min = 1,
                      max = 50,
@@ -35,19 +44,23 @@ ui <- fluidPage(
       
       # Show a plot of the generated distribution
       mainPanel(
-         textOutput("selected_var"),
-         p(),
-         #plotOutput("distPlot"),
-         plotOutput("distPlot2")
+         #textOutput("selected_var"),
+         #p(),
+         #plotOutput("distPlot")
+         plotlyOutput("distPlot2")
+        #plot_geo(df, locationmode = 'USA-states')
+        
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
     output$selected_var <- renderText({ 
       paste("You have selected", input$var)
     })
+    
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
       x    <- faithful[, 2] 
@@ -57,23 +70,22 @@ server <- function(input, output) {
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
    
-   output$distPlot2<-renderPlot({
-     gg <- ggplot()
-     gg <- gg + geom_map(data=states, map=states,
-                         aes(x=long, y=lat, map_id=region),
-                         fill="#ffffff", color="#ffffff", size=0.15)
-     gg <- gg + geom_map(data=arr, map=states,
-                         aes(fill=arrests[input$var], map_id=region),
-                         color="#ffffff", size=0.15) + guides(fill=guide_legend(title=input$var,reverse = TRUE))
-     gg <- gg + scale_fill_continuous(low='thistle2', high='darkred', 
-                                      guide='colorbar')
-     gg <- gg + labs(x=NULL, y=NULL)
-     gg <- gg + coord_map("albers", lat0 = 39, lat1 = 45) 
-     gg <- gg + theme(panel.border = element_blank())
-     gg <- gg + theme(panel.background = element_blank())
-     gg <- gg + theme(axis.ticks = element_blank())
-     gg <- gg + theme(axis.text = element_blank())
-     gg
+   output$distPlot2<-renderPlotly({
+       plot_geo(df, locationmode = 'USA-states') %>%
+       add_trace(
+         z = ~Murder, text = ~hover, locations = ~Code,
+         color = ~Murder, colors = 'Purples'
+       ) %>%
+       colorbar(title = "Millions USD") %>%
+       layout(
+         title = '2011 US Agriculture Exports by State<br>(Hover for breakdown)',
+         geo = g
+       )
+     
+   })
+   
+   output$plot <- renderPlotly({
+     plot_ly(mtcars, x = ~mpg, y = ~wt)
    })
 
 }
